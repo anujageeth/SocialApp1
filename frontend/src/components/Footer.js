4e84ea679382bc474696e8a3bdafbd6b8e93475e
anujageeth <www.anujageeth@gmail.com>
2025-02-25 17:16:23 +0530

    Second commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,14 +8,7 @@ const Footer = () => {
     <Box pb={3}>
       <Card>
         <Typography variant="subtitle1">
-          Don't forget to star the{" "}
-          <a
-            href="https://github.com/ihtasham42/social-media-app"
-            target="_blank"
-          >
-            Repo
-          </a>
-          ! ⭐
+          Copyright © 2025 by PostIt
         </Typography>
       </Card>
     </Box>
